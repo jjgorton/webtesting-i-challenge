8e4326f2841900eddd1b712b4ae135e1431de7e4
jjgorton <jjgorton@gmail.com>
2019-05-13 16:05:38 -0400

    tetsts and code added for repair() method

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,25 @@
 module.exports = {
-  succeed,
-  fail,
-  repair,
-  get,
+	succeed,
+	fail,
+	repair,
+	get
 };
 
 function succeed(item) {
-  return { ...item };
+	return { ...item };
 }
 
 function fail(item) {
-  return { ...item };
+	return { ...item };
 }
 
 function repair(item) {
-  return { ...item };
+	return {
+		...item,
+		durability : 100
+	};
 }
 
 function get(item) {
-  return { ...item };
+	return { ...item };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,24 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe('enhancer', () => {
+	describe('repair', () => {
+		it('durability set to 100', () => {
+			const item = { name: 'name', durability: 10, enhancement: 20 };
+			const actual = enhancer.repair(item);
+			expect(actual.durability).toBe(100);
+		});
+
+		it('durability set to 100 even if negative', () => {
+			const item = { name: 'name', durability: -10, enhancement: 20 };
+			const actual = enhancer.repair(item);
+			expect(actual.durability).toBe(100);
+		});
+
+		it('durability set to 100 but never over', () => {
+			const item = { name: 'name', durability: 1020, enhancement: 20 };
+			const actual = enhancer.repair(item);
+			expect(actual.durability).toBe(100);
+		});
+	});
+});
